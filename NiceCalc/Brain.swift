29e4b2e74fd526060b6545adca6dce0246bf442b
Yaroslav Luchyt <uter8764@gmail.com>
2017-07-07 18:11:44 +0300

    PN RAssoc

--- NiceCalc/Brain.swift
+++ NiceCalc/Brain.swift
@@ -12,8 +12,19 @@ class Brain/*: Model*/ {
     static let shared = Brain()
     let output = OutputAdapter.shared
     var equation = ""
-    
     var operand: String = ""
+    
+    let operation = [
+        "^": (prec: 4, rAssoc: true),
+        "√": (prec: 5, rAssoc: true),
+        "×": (prec: 3, rAssoc: false),
+        "÷": (prec: 3, rAssoc: false),
+        "+": (prec: 2, rAssoc: false),
+        "−": (prec: 2, rAssoc: false),
+        "sin": (prec: 5, rAssoc: true),
+        "cos": (prec: 5, rAssoc: true),
+        "ln": (prec: 4, rAssoc: true),
+    ]
     
     func input(_ number: Int) {
         equation += "\(number)"
@@ -32,6 +43,48 @@ class Brain/*: Model*/ {
     
     func input(operation: Operation) {
         
+    }
+    
+    func toTokens(_ equationStr: String) -> [String] {
+        let tokens = equationStr.characters.split{ $0 == " " }.map(String.init)
+        return tokens
+    }
+    
+    func toPolandNotation(tokens: [String]) -> [String] {
+        var polandNotation: [String] = []
+        var stack : [String] = [] // buffer for operation
+        for token in tokens.reversed() {
+            switch token {
+            case ")":
+                stack += [token]
+            case "(":
+                while !stack.isEmpty {
+                    if stack.removeLast() == ")" {
+                        break
+                    } else {
+                        polandNotation = [stack.removeLast()] + polandNotation
+                    }
+                }
+            default:
+                if let firstOperand = operation[token] {
+                    for op in stack.reversed() {
+                        if let secondOperand = operation[op] {
+                            if !(firstOperand.prec > secondOperand.prec || (firstOperand.prec == secondOperand.prec && firstOperand.rAssoc)) {
+                                let lastOperator = [stack.removeLast()]
+                                polandNotation = lastOperator + polandNotation
+                                continue
+                            }
+                        }
+                        break
+                    }
+                    stack += [token]
+                    
+                } else {
+                    polandNotation = [token] + polandNotation
+                }
+            }
+        }
+        return (stack + polandNotation)
     }
     
     func process() {
